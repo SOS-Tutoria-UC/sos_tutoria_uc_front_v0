1b753f35bfb518c8b8baac486967b982cce72b1c
Ana De Souza <ana.desouzag@outlook.com>
2022-08-17 08:33:32 -0400

    fixes

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from "react-router-dom"
-import Spinner from "../../../components/spinner/Spinner"
+import {Spinner} from "../../../components/spinner/Spinner"
 
 //Axios
 import instance from '../../../config/axios/instance'
