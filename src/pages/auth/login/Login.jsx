38c4c3ae3f24e64229c58abc49a52e6fa16c23f0
Ana De Souza <ana.desouzag@outlook.com>
2022-08-29 20:03:57 -0400

    Fixes

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -35,23 +35,17 @@ const Login = () => {
         "http://internetofus.u-hopper.com/prod/hub/frontend/oauth/login?client_id=NqGWkPYgkE"
       );
     } else {
-      const getToken = async () => {
-        const response = await fetch(
-          "https://internetofus.u-hopper.com/prod/api/oauth2/token",
-          {
-            method: "POST",
-            mode: "no-cors",
-            body: new URLSearchParams({
-              grant_type: "authorization_code",
-              client_id: "NqGWkPYgkE",
-              client_secret: "fELXQpoBnnMoWDgt5mek",
-              code: code,
-            }),
-          }
-        );
-        console.log(response)
-      };
-      getToken();
+      axios.post(
+          'https://internetofus.u-hopper.com/prod/api/oauth2/token',
+          new URLSearchParams({
+              'grant_type': 'authorization_code',
+              'client_id': 'NqGWkPYgkE',
+              'client_secret': 'fELXQpoBnnMoWDgt5mek',
+              'code': code
+          })
+      ).then((res) => {
+        console.log(res)
+      });
     }
   }, []);
 
