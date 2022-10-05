056a8cdcb6be51b848ee502b87ba00cf216ee8c6
Ana De Souza <ana.desouzag@outlook.com>
2022-10-05 13:13:31 -0300

    Fix

--- src/pages/user/requests/tutoring/Tutoring.jsx
+++ src/pages/user/requests/tutoring/Tutoring.jsx
@@ -84,12 +84,12 @@ const Tutoring = (props) => {
     if (positionOA === "anywhere") {
       setPositionOA("nearby");
       setHidden("hidden");
-      setDisponibilidadCheck(false)
+      setDisponibilidadCheck(false);
     } else {
-      setPositionOA("anywhere")
+      setPositionOA("anywhere");
       setHidden("");
-      setDisponibilidadCheck(true)
-    };
+      setDisponibilidadCheck(true);
+    }
   };
 
   const getBeliefAndValue = (beliefAndValue) => {
@@ -348,26 +348,30 @@ const Tutoring = (props) => {
             </label>
           </div>
           <div className="form-group mb-6">
-            <label className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-400 text-bold">
-              Elija los horarios disponibles que tiene
-            </label>
+            {disponibilidadCheck && (
+              <>
+                <label className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-400 text-bold">
+                  Elija los horarios disponibles que tiene
+                </label>
 
-           { disponibilidadCheck && <div className="flex items-center mb-4">
-              <input
-                id="disponibilidad"
-                disabled={loading}
-                type="checkbox"
-                value=""
-                className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                onChange={handleChangeDisponibilidad}
-              />
-              <label
-                for="disponibilidad"
-                className="ml-2 2xl font-medium text-gray-900 dark:text-gray-300"
-              >
-                Tengo disponibilidad para la tutoría en cualquier horario
-              </label>
-            </div>}
+                <div className="flex items-center mb-4">
+                  <input
+                    id="disponibilidad"
+                    disabled={loading}
+                    type="checkbox"
+                    value=""
+                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    onChange={handleChangeDisponibilidad}
+                  />
+                  <label
+                    for="disponibilidad"
+                    className="ml-2 2xl font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    Tengo disponibilidad para la tutoría en cualquier horario
+                  </label>
+                </div>
+              </>
+            )}
             <div
               className={`relative overflow-x-auto shadow-md sm:rounded-lg ${hidden}`}
             >
