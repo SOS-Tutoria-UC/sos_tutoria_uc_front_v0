63ad34891d6e79f1469f049c7326d2e823e1c8a8
Laura Achón <lcag2201@hotmail.com>
2022-10-20 23:19:19 -0300

    cambia Quienes Somos

--- src/pages/user/main/QuienesSomos.jsx
+++ src/pages/user/main/QuienesSomos.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const QuienesSomos = () => {
   return (
     <div className="m-5 p-5 bg-white">
-      <h1>“WeNet: Internet of us”</h1>
+      <h1><b>“WeNet: Internet of us”</b></h1>
       <p>
       <span className="font-bold">“WeNet: Internet of us”</span> es un proyecto internacional y multidisciplinario
       financiado por la Unión Europea que hace uso de la informática, la
@@ -11,6 +11,7 @@ export const QuienesSomos = () => {
       las personas para mejorar su bienestar y la calidad de las interacciones. La diversidad es parte de nuestra vida cotidiana. 
       La tecnología, a pesar de su evolución, aún no puede desarrollar y mantener relaciones sociales que trascienden los contextos geográficos y culturales.
       </p>
+      <br></br>
       <p>
       WeNet aborda esta brecha al proporcionar un paradigma de relaciones sociales mediado por máquinas y consciente de la diversidad. 
       El objetivo es conectar a personas que puedan ayudarse mutuamente, y la clave es aprovechar su diversidad.
@@ -24,13 +25,14 @@ export const QuienesSomos = () => {
       (industrias creativas, diagnóstico médico, entre otros). El consorcio WeNet desarrollará una infraestructura de investigación para explotar los resultados del 
       proyecto y fortalecer el ecosistema de innovación europeo con una perspectiva mundial.
       </p>
-      <h1>WeNet en la UC Paraguay</h1>
+      <h1><b>WeNet en la UC Paraguay</b></h1>
       <p>
-      En el marco del proyecto, se llevó a cabo en varias instituciones de educación superior del mundo, y entre ellas la Universidad Católica "Nuestra Señora de la Asunción''
+      En el marco del proyecto, se llevó a cabo en varias instituciones de educación superior del mundo, y entre ellas la Universidad Católica "Nuestra Señora de la Asunción"
        (UC), el piloto “Chatbot: Ask For Help” con el fin de estudiar la diversidad en el contexto de interacciones mediadas por la tecnología. 
        En esta experiencia los usuarios podían hacer preguntas, responder preguntas de otros usuarios, elegir la mejor respuesta a sus preguntas y 
        también ganar insignias según los tipos y niveles de participación que tenían en la aplicación. 
       </p>
+      <br></br>
       <p>
       El grupo de investigación de la UC - Paraguay además realizó una aplicación más orientada a necesidades puntuales de los alumnos, 
       y de esta manera surgió la primera versión de “SOS TUTORÍA UC”, un chatbot en Telegram que permitió a los estudiantes solicitar o 
@@ -40,7 +42,8 @@ export const QuienesSomos = () => {
         Esta es la versión II del la aplicación “SOS TUTORÍA UC”, que busca conectar alumnos de diferentes carreras o campus donde cada alumno registrado puede ayudar 
         o ser ayudado por la comunidad por medio de solicitudes de tutorías que serán llevadas a cabo por fuera de la aplicación.<span className="font-bold"> ÉXITOS.</span> 
       </p>
-      <a href="https://www.internetofus.eu/" target="_blank">Más sobre <i><b>WeNet</b></i></a>
+      <br></br>
+      <a href="https://www.internetofus.eu/" target="_blank">Click aquí para saber más sobre <i><b>WeNet</b></i></a>
     </div>
     
     
