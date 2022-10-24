8e180c46a22f0fd02a945e5cf4d24becb1cec006
Laura Achón <lcag2201@hotmail.com>
2022-10-23 23:14:12 -0300

    cambia Como interactuar

--- src/pages/user/main/ComoInteractuar.jsx
+++ src/pages/user/main/ComoInteractuar.jsx
@@ -3,13 +3,55 @@ import React from "react";
 export const ComoInteractuar = () => {
   return (
     <div className="m-5 p-5 bg-white">
-      Bienvenid@, en esta aplicación podrás pedir ayuda sobre alguna competencia
+      <span className="font-bold"> Bienvenid@!</span>, en esta aplicación podrás pedir ayuda sobre alguna competencia
       específica y te daremos una lista de posibles compañeros que encajen con
       tu necesidad (presencial o virtual), los compañeros que encajen con tus
       especificaciones recibirán una notificación con tus especificaciones y
       decidirán si aceptan el desafío o rechazan. De la misma forma, tú podrás
       recibir notificaciones de pedidos de ayuda en las competencias que
       reportaste que son tu fuerte. Éxitos!
+      <h1>
+        <b>Registro</b>
+      </h1>
+      <p>
+        P1
+      </p>
+      <h1>
+        <b>Inicio de sesión</b>
+      </h1>
+      <p>
+        P2
+      </p>
+      <h1>
+        <b>Completar el survey</b>
+      </h1>
+      <p>
+        P3
+      </p>
+      <h1>
+        <b>Solicitar tutoría</b>
+      </h1>
+      <p>
+        P4
+      </p>
+      <h1>
+        <b>Tutorías Solicitadas</b>
+      </h1>
+      <p>
+        P5
+      </p>
+      <h1>
+        <b>Tutorías Recibidas</b>
+      </h1>
+      <p>
+        P6
+      </p>
+      <h1>
+        <b>Cerrar sesión</b>
+      </h1>
+      <p>
+        P7
+      </p>
     </div>
   );
 };
