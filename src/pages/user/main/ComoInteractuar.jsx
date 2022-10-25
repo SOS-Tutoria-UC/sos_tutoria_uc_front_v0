6cc232acf33a02d3e77de116c66df0464eed3543
Laura Achón <lcag2201@hotmail.com>
2022-10-25 00:11:00 -0300

    cambia Como Interactuar

--- src/pages/user/main/ComoInteractuar.jsx
+++ src/pages/user/main/ComoInteractuar.jsx
@@ -13,71 +13,67 @@ export const ComoInteractuar = () => {
       <h1>
         <b>Registro</b>
       </h1>
-      <a href="https://tutoria-uc.vercel.app/" target="_blank" rel="noreferrer">
-        Ingresando en {" "} nos encontraremos con la siguiente página de inicio ..
-        <i>
-          <b>WeNet</b>
-        </i>
-      </a>
-      <a>
-        {" "}
-        <img
-          alt="Inicio de sesión"
-          src="whatsapp.jpg"
-          width={40}
-          height={40}
-        />
-        </a>
       <p>
-        P1
+      <span className="font-bold">Paso 1. </span> En caso que no se tenga una cuenta en WE NET, al ingresar al link de SOS TUTORÍA UC uno podrá registrarse en la App. Si se cuenta con una cuenta de WE NET ya asociada a la App, saltar al Paso 3.
+      </p>
+      <p>
+      <span className="font-bold">Paso 2. </span> Al completar el Paso 1, deberá redireccionarse al link que llegará por mail para completar algunos datos en la página de WE NET.
       </p>
       <h1>
         <b>Inicio de sesión</b>
       </h1>
       <p>
-        P2
+      <span className="font-bold">Paso 3. </span> Ya con un usuario y contraseña válidos, iniciamos sesión en la App para continuar con el Consentimiento Informado.
       </p>
       <h1>
         <b>Completar el survey</b>
       </h1>
       <p>
-        P3
+      <span className="font-bold">Paso 4. </span> Si es la primera vez que inicia sesión en la App, deberá completar el Survey cuya opción se encuentra en la primera opción en la pestaña superior izquierda de la pantalla. 
+      Esta información es muy importante y nos ayudará a poder recomendar y recomendarte mejor alumnos en cada solicitud que crees posteriormente.
+      Si no es la primera vez que inicia sesión y ya completaste el Survey, salta al Paso 5.
+      </p>
+      <p>
+      En este punto tenemos la opción de realizar una solicitud de tutoría (Paso 5), ver las solicitudes realizadas (Paso 6), contestar solicitudes recibidas (Paso 7), evaluar a un tutor (Paso 8) o evaluar a un alumno (Paso 9).
       </p>
       <h1>
         <b>Solicitar tutoría</b>
       </h1>
       <p>
-        P4
+      <span className="font-bold">Paso 5. </span> En caso que quiera hacer una nueva solicitud deberá tener en cuenta lo siguiente:
+      
       </p>
       <h1>
         <b>Tutorías Solicitadas</b>
       </h1>
       <p>
-        P5
+      <span className="font-bold">Paso 6. </span> En caso que quiera hacer una nueva solicitud
       </p>
       <h1>
         <b>Tutorías Recibidas</b>
       </h1>
       <p>
-        P6
+      <span className="font-bold">Paso 7. </span> En caso que quiera hacer una nueva solicitud
       </p>
       <h1>
         <b>Evaluar tutor</b>
       </h1>
+      <p>Este paso es importante en caso que tú u otro compañero quiera acceder a créditos académicos o gratificaciones posteriores.</p>
       <p>
-        P8
-      </p>    
+      <span className="font-bold">Paso 8. </span> En caso que quiera hacer una nueva solicitud
+      </p>  
       <h1>
         <b>Evaluar alumno</b>
       </h1>
+      <p>Este paso es importante en caso que tú u otro compañero quiera acceder a créditos académicos o gratificaciones posteriores.</p>
       <p>
-        P9
-      </p> 
+      <span className="font-bold">Paso 9. </span> En caso que quiera hacer una nueva solicitud
+      </p>
       <h1>
         <b>Cerrar sesión</b>
       </h1>
       <p>
-        P7
+        En la esquina superior derecha se encuentra el ícono del usuario, haciendo click en este se podrá cerrar sesión en la App.
       </p>
     </div>
   );
