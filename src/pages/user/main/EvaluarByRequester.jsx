5939bc2925142b9e933f5f8f9f6e6047909aad55
Ana De Souza <ana.desouzag@outlook.com>
2022-11-25 21:57:58 -0300

    Fix

--- src/pages/user/main/EvaluarByRequester.jsx
+++ src/pages/user/main/EvaluarByRequester.jsx
@@ -199,7 +199,21 @@ const EvaluarByRequester = () => {
             )}
             {!tutoria && (
               <div className="w-full px-3 mb-6">
-                <SelectQs05 disabled={loading} />
+                <label
+                  for="message"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  ¿Por qué no se realizó la tutoría?
+                </label>
+                <textarea
+                  id="motivo"
+                  rows="4"
+                  disabled={loading}
+                  value={motivo}
+                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Escribir motivo"
+                  onChange={(e) => setMotivo(e.target.value)}
+                ></textarea>
               </div>
             )}
           </div>
