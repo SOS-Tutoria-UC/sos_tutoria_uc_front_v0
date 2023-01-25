e1407c5200e62dd95aaa6c65781d40d99c4ea65f
Ana De Souza <ana.desouzag@outlook.com>
2023-01-25 12:37:21 -0300

    Prueba de spinner con mensaje

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -1,21 +1,27 @@
 import React from "react";
-import './spinner.css';
+import "./spinner.css";
 
 export const Spinner = () => {
   return (
-    <div className="sk-circle">
-      <div className="sk-circle1 sk-child"></div>
-      <div className="sk-circle2 sk-child"></div>
-      <div className="sk-circle3 sk-child"></div>
-      <div className="sk-circle4 sk-child"></div>
-      <div className="sk-circle5 sk-child"></div>
-      <div className="sk-circle6 sk-child"></div>
-      <div className="sk-circle7 sk-child"></div>
-      <div className="sk-circle8 sk-child"></div>
-      <div className="sk-circle9 sk-child"></div>
-      <div className="sk-circle10 sk-child"></div>
-      <div className="sk-circle11 sk-child"></div>
-      <div className="sk-circle12 sk-child"></div>
-    </div>
+    <>
+      {" "}
+      <div className="sk-circle">
+        <div className="sk-circle1 sk-child"></div>
+        <div className="sk-circle2 sk-child"></div>
+        <div className="sk-circle3 sk-child"></div>
+        <div className="sk-circle4 sk-child"></div>
+        <div className="sk-circle5 sk-child"></div>
+        <div className="sk-circle6 sk-child"></div>
+        <div className="sk-circle7 sk-child"></div>
+        <div className="sk-circle8 sk-child"></div>
+        <div className="sk-circle9 sk-child"></div>
+        <div className="sk-circle10 sk-child"></div>
+        <div className="sk-circle11 sk-child"></div>
+        <div className="sk-circle12 sk-child"></div>
+      </div>
+      <h5 className="text-3xl leading-6 font-bold text-red-900 mb-4">
+        Por favor, estamos buscando tutores para tí...
+      </h5>
+    </>
   );
 };
